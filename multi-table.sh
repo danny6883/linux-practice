5a5ebf20dd8b58c1f098c9c4caecbac5710b7ff1
danny <danny756883@gmail.com>
2021-09-27 17:01:46 +0900

    Update multi-table

--- multi-table.sh
+++ multi-table.sh
@@ -1,4 +1,18 @@
-#!/bin/sh
+#!/bin/bash
+re="^[1-9]+[0-9]*$"
+if [[ $1 =~ $re ]]
+then :
+else
+  echo "wrong input"
+  exit 1
+fi
+if [[ $2 =~ $re ]]
+then :
+else
+  echo "wrong input"
+  exit 1
+fi
+
 for i in $(seq  $1)
 do
   for j in $(seq $2)
